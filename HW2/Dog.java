38b2f186bdd2c13c8a14258f4ef85750b095e6d2
Oleksii Klym <130431564+Flayerich@users.noreply.github.com>
2023-10-16 12:30:08 +0200

    Add files via upload

---
+++ HW2/Dog.java
@@ -0,0 +1,42 @@
+package hw2;
+
+class Dog extends Animal {
+	
+	private String name;
+	
+	public Dog(String ration, String color, int weight, String name) {
+		super(ration, color, weight);
+		this.name = name;
+	}
+	
+	public Dog() {
+		super();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	 
+	@Override
+	    public String getVoice() {
+	        return "Гав-Гав";
+	    }
+	
+	@Override
+	public void eat() {
+		System.out.println("Собачка їсть");
+	}
+	@Override
+	public void sleep() {
+		System.out.println("Собачка спить");
+	}
+	
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + "]";
+	}
+	
+}
